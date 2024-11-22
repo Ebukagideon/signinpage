69063d6f2cd82489936dce43c9a586c1a957aee2
Ebuka <gideonebuka6@gmail.com>
2024-11-22 20:35:00 +0000

    4 commit

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
   return (
     <div className="mx-auto bg-white h-screen md:flex  text-mine">
       <div className='hidden sm:block h-screen bg-cover w-2/5 m-1 rounded'>
-        <img src="/image/onboarding-slider.png" alt='' className='sm:h-100 object-cover '/>
+        <img src="/image/onboarding-slider.png" alt='' className='object-cover '/>
       </div>
 
-      <div className="text-center flex-1 flex-col font-sans text-sm">
-        <div className='justify-center lg:mx-52 mb-30 my-32'>
+      <div className="text-center justify-center flex-1 flex-col font-sans text-sm">
+        <div className='justify-center md:mt-40 md:mb-32 md:mx-20'>
           <div>
             <h1 className='text-2xl font-bold text-black'>Welcome back</h1>
             <p>Enter your details to login</p>
@@ -42,8 +42,9 @@ function App() {
               value={email}
               required
               aria-describedby="uidnote"
-              className='border mr-2 w-full h-10 rounded-lg p-1'
+              className='border w-full h-10 rounded-lg p-2'
               />
+              
 
               <p>Password</p>
               <input
@@ -53,7 +54,7 @@ function App() {
               value={pwd}
               required
               aria-describedby="pwdnote"
-              className='border mr-2 w-full h-10 rounded-lg p-1'/>
+              className='border w-full h-10 rounded-lg p-2'/>
 
               <div className='flex justify-between'>
                 <p>Remember me</p>
@@ -63,12 +64,12 @@ function App() {
 
             <div className='w-full pt-5'>
               <button disabled={!validEmail || !validPwd ? true : false} className='bg-btn w-full h-10 rounded-lg text-white'>Log in</button>
-              <p>Dont have an account? <a href="" className='text-green-700'>Signup</a></p>
+              <p>Dont have an account? <a href="" className='text-btn'>Signup</a></p>
             </div>
           </form>
         </div>
-        <div className=' lg:px-52'>
-          <p className='text-xs'>By clicking “Login or Signup”, you assert that you have read and <br/>agreed to our Terms of Service and Privacy Policy.</p>
+        <div className='w-full '>
+          <p className='text-xs'>By clicking “Login or Signup”, you assert that you have read and <br/>agreed to our <a href="" className='text-btn'>Terms of Service</a> and <a href="" className='text-btn'>Privacy Policy.</a></p>
         </div>
       </div>
     </div>
